b586d7150a208d04338fa547a539e4111bdcddf3
Trishelle Lobin <trishellelobin2005@gmail.com>
2025-02-18 08:22:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+let name = "bob";
+let age = 24;
+
+console.log(typeof(name))//string
+console.log(typeof(age))//number
+
+console.log(`Hello my name is ${name}, I'm ${age} years old`);
+//Hello my name is bob, I'm 24 years old.
+
+console.log(`I was born in ${2020 - age}`)
+//I was born in 1966 
+/*let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
 
 for(let i=0; i< arr.length; i++){
   console.log(arr[i])
 }
+  */
