b84dfb35cc926bac4a9ef17328d2bc95a35d5f58
Trishelle Lobin <trishellelobin2005@gmail.com>
2025-02-25 19:01:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,72 @@
-//object literal
-//submit for lab 5
-getAverateGrade(student, course){
-  
+
+function getAverageGrade(student, course){
+  let sum=0;
+  let c=0;
+    for(let person of student.transcript){
+      if(person.course===course){
+for(let grade of person.grades){
+      sum += grade;
+      c=c+1;
+    }
+
+  }
 }
+if (c===0){
+
+return -1;
+}
+return sum/c;
+}
+
+
+function getAssignmentMark(student, course, num){
+let sum = 0;
+let count = 0;
+
+for (let student of students) {
+  for (let person of student.transcript) {
+    if (person.course === course) {
+      let i = 0;
+      for (let grade of person.grades) {
+        if (i === num) {
+          sum += grade;
+          count += 1;
+        }
+        i += 1;
+      }
+    }
+  }
+}
+
+if (count === 0) {
+  return -1;
+}
+
+return sum / count;
+}
+
+function averageAssessment(students, courseName, assignment){
+  let sum = 0;
+  let count = 0;
+
+  for (let student of students) {
+    for (let person of student.transcript) {
+      if (person.course === courseName) {
+        for (let grade of person.grades) {
+          sum += grade;
+          count += 1;
+        }
+      }
+    }
+  }
+
+  if (count === 0) {
+    return -1;
+  }
+  return sum / count;
+}
+
+
 let bob = {
   fname: "bob",
   lname: "smith",
@@ -49,151 +113,3 @@ let paul = {
 
 const students = [bob, sally, paul];
 
-/*//Create a constructor a functions which builds object for us
-function createPerson(name, height, weight) {
-  return { name: name, height: height, weight: weight };
-}
-
-function calcBMI(weight, height) {
-  return weight / (height * height);
-}
-
-function avgBMI(people) { //only name is specified
-  let sum = 0;
-  for (let person of people) {
-    //sum the bmi of each person
-    sum += calcBMI(person.weight, person.height);
-  }
-  //calculate average
-  return sum / people.length;
-}
-
-//create a collection of people
-let people = [
-  createPerson("Sally", 60, 2.5),
-  createPerson("Ben", 81, 3),
-  createPerson("Shelly", 50, 1.7)
-];
-
-console.log(avgBMI(people));
-*/
-/*let person = {
-  // key : value
-  "name": "John",
-  "age": 12
-};
-
-//we can print the values of a key in two ways
-console.log(person["age"]);//Using an index
-console.log(person.name);//referencing the key as a property
-
-person["weight"] = 70;
-
-person.marks = [67, 34, 55, 89];
-
-let firstMark = person.marks[0];
-
-//mixing array and objects
-let people = [
-  person,
-  {
-    name: "Jane",
-    age: 23,
-    marks: [51, 78, 99, 76]
-  }
-];
-
-let johnClone = {};//empty object
-
-//copying objects
-Object.assign(johnClone, person);
-
-console.log(johnClone);
-//multiple datatypes in an array
-
-console.log(people[0].marks[0]);//what is printed?
-*/
-/*
-let arr = [12, 33, 4, 5, -4, 8, 19, 25];
-                
-//map() creates a new array from the elements of one without changing the old one
-function double(num){
-  return num * 2;
-}
-
-let doubledArr = arr.map(double); 
-console.log(doubledArr);
-
-function isOdd(num){
-  return num%2 !== 0; 
-}
-//Filter takes a test condition and returns only the element which 
-//make the condition true
-let odds = arr.filter(isOdd);
-console.log(odds);
-
-//Returns true or false if any of the elements of the array 
-//meets a specified condition
-function has5Factor(num){
-  return num % 5 === 0;
-}
-
-let hasFiveFactor = arr.some(has5Factor);
-console.log(hasFiveFactor);
-
-function intCompare(a, b){
- return a - b;
-}
-
-//sort function must return either 0, +ve, -ve
-let ascending = arr.sort(intCompare)
-console.log(ascending);
-*/
-/*
-let arr = [ -5, 16, 33, 42, 103, 344];
-
-console.log(arr.includes(-5));//true
-
-arr.push(11);//adds item to the end
-
-console.log(arr)//[ -5, 16, 33, 42, 103, 344, 11];
-
-let lastItem = arr.pop();//removes last item
-
-console.log(lastItem, arr);//11, [ -5, 16, 33, 42, 103, 344]
-
-arr.unshift(22);//adds item to the front
-
-console.log(arr);//[22, -5, 16, 33, 42, 103, 344]
-
-let firstItem = arr.shift();//removes first item
-console.log(firstItem, arr);//22, [-5, 16, 33, 42, 103, 344]
-
-let reversed = arr.reverse();//creates a new array in reverse order 
-console.log(reversed);//[344, 103, 42, 33, 16, -5]
-console.log(arr.join('-'));//"-5-103-16-33-344-42" joins array with provided separator
-*/
-/*
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
-
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
-}
-
-for (let item of arr){// for of loop
-   console.log(item);
-}
- */
- /*
- function printDate(){
-  console.log(new Date().toLocaleTimeString());
-}
-     
-setInterval(printDate, 1000);
- */
-/*let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
-
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
-}
-  */
